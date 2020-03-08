82ce767f67eb55e2cbd3f6a6d5212f051a42c9f9
Vanessa0122 <yp4@ualberta.ca>
2020-03-08 10:57:17 -0600

    Change login and register. We have token now

--- src/Pages/Post.js
+++ src/Pages/Post.js
@@ -1,8 +1,4 @@
-
-
 import React from 'react';
-
-
 
 class Post extends React.Component{
     constructor(props){
@@ -12,24 +8,13 @@ class Post extends React.Component{
             comments:null,
         }
     }
-
-   
     
-
     render(){
-    // let request = new XMLHttpRequest()
-    // request.open('GET', this.props.location.state.user)
-    // request.send()
-    // request.onload = () => {
-    //     let res = JSON.parse(request.response)
-        
-    //     alert(JSON.stringify(res,null,4))
 
-    // }
-    return(
-        <div>{this.props.location.state.user}<br></br>
-        {this.props.location.state.text}</div>
-    )
+        return(
+            <div>{this.props.location.state.user}<br></br>
+            {this.props.location.state.text}</div>
+        )
     }
 
 
